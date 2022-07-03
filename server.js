8f67e67ba322f6bdcd474e31dfe577e8f15b2dbe
Amir <amir.redjem@etu.univ-amu.fr>
2022-07-04 00:43:00 +0200

    Debut de conception du site

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express')
+const mustache = require('mustache-express');
+const anime = require('animejs');
+
+const app = express()
+
+app.engine('html', mustache());
+app.set('view engine', 'html');
+app.set('views', './views');
+app.use(express.static('views'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.listen(8000, () => console.log('Application Web ! http://localhost:8000'));
+
