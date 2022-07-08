8efe72de77432e589a4a6324dcf4e749adbae0ff
REDJEM-Amir <lepetitgolum@gmail.com>
2022-07-08 20:48:27 +0200

    Mise a jour majeur accueil

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const mustache = require('mustache-express');
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true }));
+
 app.engine('html', mustache());
 app.set('view engine', 'html');
 app.set('views', './views');
